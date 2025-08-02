cd84331d25b6afe0e913df80d3a23341843d3e73
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2025-08-03 01:14:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ BOT_TOKEN = "ISI_TOKEN_BOT"
 ADMIN_ID = 123456789  # ganti dengan Telegram ID kamu
 MEDIA_FOLDER = "assets/media"
 BROADCAST_HOURS = [7, 11, 15, 19, 22]  # contoh 5x sehari
+
+# Konfigurasi PostgreSQL
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_NAME = "fluxiondb"
+DB_USER = "postgres"
+DB_PASSWORD = "passwordmu"
